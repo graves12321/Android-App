9ca2b45a5093a38266e0de1343a0abdc1de318d8
Võ Thành Luân <71899498+graves12321@users.noreply.github.com>
2020-09-26 01:57:50 +0700

    first commit

---
+++ app/src/main/java/com/cmajor7/signup/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cmajor7.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
